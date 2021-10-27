19a2daa031ac03dfc2b9d1f14248c2e7d5714fa5
Natalia Romashko <sharkellgm@gmail>
2021-10-27 15:46:17 +0300

    feat: add hover effect on header tabs

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,31 +1,61 @@
+import { useRef } from 'react';
 import { NavLink } from 'react-router-dom';
 import './header.css';
 
-export const Header = () => (
-  <div className='header-container'>
-    <NavLink
-      exact
-      to='/'
-      className='header-link'
-      activeClassName='header-link-active'
-    >
-      Home
-    </NavLink>
-    <NavLink
-      exact
-      to='/countdown-timer'
-      className='header-link'
-      activeClassName='header-link-active'
-    >
-      Countdown Timer
-    </NavLink>
-    <NavLink
-      exact
-      to='/markdown-editor'
-      className='header-link'
-      activeClassName='header-link-active'
-    >
-      Markdown Editor
-    </NavLink>
-  </div>
-);
+export const Header = () => {
+  const btnHome = useRef<HTMLAnchorElement>(null);
+  const btnMarkdown = useRef<HTMLAnchorElement>(null);
+  const btnCountdown = useRef<HTMLAnchorElement>(null);
+
+  const onMousePosition = (e: any) => {
+    const left = (e.nativeEvent.offsetX / e.target.clientWidth) * 100 + '%';
+    const top = (e.nativeEvent.offsetY / e.target.clientHeight) * 100 + '%';
+    if (btnHome.current && e.target === btnHome.current) {
+      btnHome.current.style.setProperty('--left', left);
+      btnHome.current.style.setProperty('--top', top);
+    }
+    if (btnCountdown.current && e.target === btnCountdown.current) {
+      btnCountdown.current.style.setProperty('--left', left);
+      btnCountdown.current.style.setProperty('--top', top);
+    }
+    if (btnMarkdown.current && e.target === btnMarkdown.current) {
+      btnMarkdown.current.style.setProperty('--left', left);
+      btnMarkdown.current.style.setProperty('--top', top);
+    }
+  };
+
+  return (
+    <div className='header-container'>
+      <NavLink
+        exact
+        to='/'
+        className='header-link'
+        activeClassName='header-link-active'
+        onMouseMove={(e) => onMousePosition(e)}
+        ref={btnHome}
+      >
+        <div className='link-content'>Home</div>
+      </NavLink>
+      <NavLink
+        exact
+        to='/countdown-timer'
+        className='header-link'
+        activeClassName='header-link-active'
+        onMouseMove={(e) => onMousePosition(e)}
+        ref={btnCountdown}
+      >
+        Countdown
+      </NavLink>
+      <NavLink
+        exact
+        to='/markdown-editor'
+        className='header-link'
+        activeClassName='header-link-active'
+        onMouseMove={(e) => onMousePosition(e)}
+        ref={btnMarkdown}
+      >
+        Markdown
+      </NavLink>
+    </div>
+  );
+};
